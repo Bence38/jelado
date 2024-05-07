c30b6b41d47d85432bf4c639052b070319db7605
Rbencuska69 <sutkabence2006@gmail.com>
2024-05-07 14:18:26 +0200

    3.feladat

--- jelado.py
+++ jelado.py
@@ -18,6 +18,13 @@ for sor in f:
     sor = sor.strip().split(" ")
     lista.append(Jel(sor[0],sor[1],sor[2],sor[3],sor[4]))
     
-
+print('2.feladat')
 sorszam = int(input("Adjon meg egy sorszámot: "))
 print (f"x={lista[sorszam-1].x}  y={lista[sorszam-1].y}")
+
+print('3.feladat')
+def eltelt(elso,masodik):
+    i1 = elso[0] + 3600 + elso[1] * 60 + masodik[2]
+    i2 = elso[0] + 3600 + elso[1] * 60 + masodik[2]
+    elt = i2-i1
+    return elt
